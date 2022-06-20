b25b87264d3bcd90705000b13417267851222afd
mbeniac <mandybeniac@gmail.com>
2022-06-20 16:31:49 -0500

    set up

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">Dictionary Search</header>
+    </div>
+  );
+}
+
+export default App;
