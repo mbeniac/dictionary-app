fbc58755ec7f2adf8a0ad4a451dc60851285eaf0
mbeniac <mandybeniac@gmail.com>
2022-06-20 16:49:32 -0500

    added footer and link

--- src/App.js
+++ src/App.js
@@ -3,7 +3,13 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">Dictionary Search</header>
+      <div className="container">
+        <header className="App-header">Dictionary Search</header>
+        <footer>
+          Coded by Mandy Beniac with
+          <a href="https://github.com/mbeniac/dictionary-app"> GitHub</a>.
+        </footer>
+      </div>
     </div>
   );
 }
