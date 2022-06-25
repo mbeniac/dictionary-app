52e9acb741bd54a6f505b419ba99b6a1cc67062d
mbeniac <mandybeniac@gmail.com>
2022-06-24 21:53:07 -0500

    dictionary element

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
       <div className="container">
         <header className="App-header">Dictionary Search</header>
+        <main>
+          <Dictionary />
+        </main>
         <footer>
           Coded by Mandy Beniac with
           <a href="https://github.com/mbeniac/dictionary-app"> GitHub</a>.
