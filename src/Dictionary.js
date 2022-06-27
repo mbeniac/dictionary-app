dae40bc185fbc1f2387492f8e79c291a8b05b0ee
mbeniac <mandybeniac@gmail.com>
2022-06-26 19:59:06 -0500

    search engine function with word alerted

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,6 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Dictionary.css";
 
 export default function Dictionary() {
-  return <div className="Dictionary">Hello from Dictionary</div>;
+  let [keyWord, setKeyword] = useState("");
+  function search(event) {
+    event.preventDefault();
+    alert(`Searching for ${keyWord}`);
+  }
+
+  function handleKeyWordChange(event) {
+    setKeyword(event.target.value);
+  }
+
+  return (
+    <div className="Dictionary">
+      <form onSubmit={search}>
+        <input type="search" autoFocus={true} onChange={handleKeyWordChange} />
+      </form>
+    </div>
+  );
 }
